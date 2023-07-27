8fa12fd57bf82b8a25c4d326682c4683d60d03aa
RashiTech <90626052+RashiTech@users.noreply.github.com>
2023-07-27 14:25:31 +0530

    Update train_test.py

--- train_test.py
+++ train_test.py
@@ -21,7 +21,7 @@ def train(model, device, train_loader, optimizer, epoch, sched, criterion, train
     scheduler = StepLR(optimizer, step_size=100, gamma=0.25)
     sched_flag = 'X'
   elif sched == 'OneCycle':
-    scheduler = OneCycleLR(optimizer=optimizer, max_lr=max_lr, epochs=epoch, steps_per_epoch=len(train_loader), pct_start=5/epoch, div_factor=10) 
+    scheduler = OneCycleLR(optimizer=optimizer, max_lr=max_lr, epochs=epoch, steps_per_epoch=len(train_loader), pct_start=5/epoch, div_factor=10,three_phase=False) 
     sched_flag = 'X'
   else:
     sched_fl = ''
